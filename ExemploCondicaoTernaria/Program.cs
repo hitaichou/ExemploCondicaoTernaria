91a63a35397a9ddf4672a51f8a66a561a01ed7ff
hitaichou <holyfernandes@yahoo.co.uk>
2020-02-24 09:15:37 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploCondicaoTernaria/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExemploCondicaoTernaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
