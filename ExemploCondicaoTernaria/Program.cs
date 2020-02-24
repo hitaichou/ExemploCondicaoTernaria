447718d169bc271c8b8557b6c384daf50025cca5
hitaichou <holyfernandes@yahoo.co.uk>
2020-02-24 09:24:34 -0300

    Exemplo Condição Ternária

--- ExemploCondicaoTernaria/Program.cs
+++ ExemploCondicaoTernaria/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ExemploCondicaoTernaria
 {
@@ -6,7 +7,25 @@ namespace ExemploCondicaoTernaria
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //if-else tradicional
+            /*
+            double desconto;
+
+            if(preco < 20.0)
+            {
+                desconto = preco * 0.1;
+            }
+            else
+            {
+                desconto = preco * 0.05;
+            }
+            */
+
+            //Expressão Condicional Ternária
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05; //se preco menor que 20, 10% de desconto, senão 5%.
+
+            Console.WriteLine("Desconto: " + desconto);
         }
     }
 }
